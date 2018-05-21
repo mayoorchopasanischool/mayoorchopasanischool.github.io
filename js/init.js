40a192c8b8c8bf006bcfd9ca974cd197d0589243
Mayoor Chopasani School <virajmathur2002@zoho.com>
2018-05-22 01:19:35 +0530

    twenty fourth (addition of educational trips links)

--- js/init.js
+++ js/init.js
@@ -10,6 +10,10 @@
     $(document).ready(function(){
       $('.collapsible').collapsible();
     });
+    $(document).ready(function(){
+      // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+      $('.modal').modal();
+    });
   }); // end of document ready
 
 })(jQuery); // end of jQuery name space
